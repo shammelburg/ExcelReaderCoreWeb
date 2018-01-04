6795d11c4774404d87224cbf2cc7ce9b63321b27
Sander Hammelburg <sander.hammelburg@powerbox.com>
2018-01-04 14:50:52 +0000

    add: comment for adding config

--- ExcelReaderCoreWeb/Startup.cs
+++ ExcelReaderCoreWeb/Startup.cs
@@ -21,6 +21,7 @@ namespace ExcelReaderCoreWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Added manually to enable file reading
             System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
 
             services.AddMvc();
@@ -33,10 +34,6 @@ namespace ExcelReaderCoreWeb
             {
                 app.UseDeveloperExceptionPage();
                 app.UseBrowserLink();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
             }
 
             app.UseStaticFiles();
